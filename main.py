f9c45ab6a591163e9bc309e965752818ab490647
chenyaofo <chenyaofo@gmail.com>
2017-12-16 19:30:51 +0800

    update

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ from PIL import Image
 # webapp
 app = Flask(__name__)
 
+def predict_with_pretrain_model(sample):
+	'''
+	Args:
+		sample: A ndarray indicating an image, which shape is (28,28).
+
+	Returns:
+		A list consists of 10 double numbers, which denotes the probabilities of numbers(from 0 to 9).
+		like [0.1,0.1,0.2,0.05,0.05,0.1,0.1,0.1,0.1,0.1].
+	'''
+	pass
 
 @app.route('/api/mnist', methods=['POST'])
 def mnist():
     input = ((numpy.array(request.json, dtype=numpy.uint8))).reshape(28, 28)
-    print(input.shape)
-    #input : (28,28) ndarray
-    #output : list 10 double numbers
-    return jsonify(results=[0.1,0.1,0.2,0.05,0.05,0.1,0.1,0.1,0.1,0.1])
+    output = predict_with_pretrain_model(input)
+    return jsonify(results=output)
 
 
 @app.route('/')
